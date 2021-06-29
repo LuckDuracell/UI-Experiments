fd0d4a4a24fbfedc8187338cd85f5e0ab8d7a48e
LuckDuracell <lukedru@yahoo.com>
2021-06-29 12:22:52 -0700

    Initial Commit

---
+++ Shared/UI_ExperimentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UI_ExperimentsApp.swift
+//  Shared
+//
+//  Created by Luke Drushell on 6/29/21.
+//
+
+import SwiftUI
+
+@main
+struct UI_ExperimentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
